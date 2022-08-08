e59a3f8f5d3ca5d55fd1edcdbea452dd1cc87ba2
woojin999 <24864@naver.com>
2022-08-08 19:14:36 +0900

    feat : 채팅뷰 페이지,리스트,전송기능

---
+++ src/main/java/com/shoebid/www/service/ChatService.java
@@ -0,0 +1,11 @@
+package com.shoebid.www.service;
+import java.util.List;
+import com.shoebid.www.domain.ChatVO;
+public interface ChatService {
+	int post(ChatVO cvo);
+	List<ChatVO> getList();
+	List<ChatVO> spread(long room);
+	ChatVO getDetail(long cno);
+	int modify(ChatVO cvo);
+	int remove(long cno);
+}
---
+++ src/main/java/com/shoebid/www/service/ChatServiceImpl.java
@@ -0,0 +1,37 @@
+package com.shoebid.www.service;
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+import com.shoebid.www.domain.ChatVO;
+import com.shoebid.www.repository.ChatDAO;
+@Service
+public class ChatServiceImpl implements ChatService {
+	@Inject
+	private ChatDAO cdao;
+	
+	@Override
+	public int post(ChatVO cvo) {
+		return cdao.insert(cvo);
+	}
+	@Override
+	public List<ChatVO> spread(long room) {
+		return cdao.selectList(room);
+	}
+	
+	@Override
+	public List<ChatVO> getList() {
+		return cdao.selectRoomList();
+	}
+	@Override
+	public int modify(ChatVO cvo) {
+		return cdao.update(cvo);
+	}
+	@Override
+	public int remove(long cno) {
+		return cdao.delete(cno);
+	}
+	@Override
+	public ChatVO getDetail(long cno) {
+		return cdao.selectOne(cno);
+	}
+}
