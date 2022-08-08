ea2dee4b2cb822633da955a3efc3486ed9df0742
eunsol-an <dev.eunsol@gmail.com>
2022-08-08 15:58:03 +0900

    refactor: 메인화면 수정 완료
    로고 추가, 메인 배너 추가, 메인화면에 상품 리스트 추가,
    내비바에서 현재 개발된 기능들 연결 작업 완료

--- src/main/java/com/shoebid/www/ctrl/HomeController.java
+++ src/main/java/com/shoebid/www/ctrl/HomeController.java
@@ -4,12 +4,18 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.shoebid.www.domain.PagingVO;
+import com.shoebid.www.handler.PagingHandler;
+import com.shoebid.www.service.ProductService;
 
 /**
  * Handles requests for the application home page.
@@ -19,19 +25,19 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Inject
+	private ProductService psv;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, PagingVO pgvo) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("list", psv.getList(pgvo));
+		int totalCount = psv.getTotalCount();
+		model.addAttribute("pgn",new PagingHandler(pgvo, totalCount));
 		
 		return "home";
 	}
