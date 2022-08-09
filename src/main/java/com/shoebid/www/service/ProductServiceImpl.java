8d405d1f54134bf737778748f1ffe77e0b9282bf
sungyeol choi <tjdfuf802@naver.com>
2022-08-09 20:00:42 +0900

    feat:상품리스트 endTime처리
    리스트페이지 접속시 상품전체리스트 불러와서 마감시간과 현재시간 비교해서
    상태변경 로직 구현.
    디테일에 경매시간 끝났을때 경매마감 알럿 삭제, 리스트 ui살짝 변경

--- src/main/java/com/shoebid/www/service/ProductServiceImpl.java
+++ src/main/java/com/shoebid/www/service/ProductServiceImpl.java
@@ -1,5 +1,10 @@
 package com.shoebid.www.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -16,6 +21,7 @@ import com.shoebid.www.repository.ImageFileDAO;
 import com.shoebid.www.repository.ProductDAO;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -23,49 +29,46 @@ public class ProductServiceImpl implements ProductService {
 	ProductDAO pdao;
 	@Inject
 	private ImageFileDAO idao;
-	
+
 	@Transactional
 	@Override
 	public int register(ProductDTO pdto) {
 		int isUp = pdao.insert(pdto.getPvo());
-			for (ImageFileVO ivo : pdto.getImageList()) {
-				long pno = pdao.selectLastPno();
-				ivo.setPno(pno);
-				isUp *= idao.insertPImage(ivo);
-			}
+		for (ImageFileVO ivo : pdto.getImageList()) {
+			long pno = pdao.selectLastPno();
+			ivo.setPno(pno);
+			isUp *= idao.insertPImage(ivo);
+		}
 		return isUp;
-		
 	}
-
+	@Transactional(isolation = Isolation.READ_COMMITTED)
 	@Override
 	public List<ProductVO> getList(PagingVO pgvo) {
+		getAllList();
 		return pdao.selectList(pgvo);
 	}
+ 
 	@Transactional(isolation = Isolation.READ_COMMITTED)
 	@Override
 	public ProductDTO getDetail(long pno) {
 		int isUp = pdao.updateReadCount(pno, 1);
 		String nick_name = pdao.selectNickName(pno);
 		int maxPrice = pdao.selectMaxPrice(pno);
-		
-		List<ImageFileVO> imageList= idao.selectListPImage(pno);
-		return new ProductDTO(pdao.selectDetail(pno),
-				maxPrice, 
-				nick_name, 
-				imageList);
+
+		List<ImageFileVO> imageList = idao.selectListPImage(pno);
+		return new ProductDTO(pdao.selectDetail(pno), maxPrice, nick_name, imageList);
 	}
+
 	@Transactional
 	@Override
 	public int modify(ProductDTO pdto) {
 		int isUp = pdao.updateReadCount(pdto.getPvo().getPno(), -2);
-		isUp =pdao.update(pdto.getPvo());
-		if(pdto.getImageList() != null ) {
-			log.info(">>>>수정파일 있을경우 서비스");
-				isUp = idao.deleteAllPImage(pdto.getPvo().getPno());
-				for (ImageFileVO ivo :pdto.getImageList() ) {
-					ivo.setPno(pdto.getPvo().getPno());
-					isUp *= idao.insertPImage(ivo);
-					log.info(">>>>수정파일 있을경우 파일서버등록"); 
+		isUp = pdao.update(pdto.getPvo());
+		if (pdto.getImageList() != null) {
+			isUp = idao.deleteAllPImage(pdto.getPvo().getPno());
+			for (ImageFileVO ivo : pdto.getImageList()) {
+				ivo.setPno(pdto.getPvo().getPno());
+				isUp *= idao.insertPImage(ivo);
 			}
 		}
 		return isUp;
@@ -73,9 +76,9 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public int remove(long pno) {
-		int isUp =pdao.delete(pno);
-		if(isUp> 0) {
-		 isUp = idao.deleteAllPImage(pno);
+		int isUp = pdao.delete(pno);
+		if (isUp > 0) {
+			isUp = idao.deleteAllPImage(pno);
 		}
 		return isUp;
 	}
@@ -93,8 +96,30 @@ public class ProductServiceImpl implements ProductService {
 	@Override
 	public int statusProduct(ProductVO pvo) {
 		
-		
 		return pdao.updateStatus(pvo);
+	}
+
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	private void getAllList() {
+		List<ProductVO> list = pdao.selectAllList();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for (ProductVO pvo : list) {
+			try {
+				Date now = new Date();
+				Date endTime = df.parse(pvo.getEndTime());
+				int result = now.compareTo(endTime);
+				if (result > 0) {
+					if (pvo.getHighestPrice() > 0) {
+						pvo.setStatus(1);
+					} else {
+						pvo.setStatus(2);
+					}
+					pdao.updateStatus(pvo);
+				}
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
